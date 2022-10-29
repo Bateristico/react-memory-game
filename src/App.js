bcab4e3c1e6d68fc8ac5e57a1220415ba5ac641e
Camilo Saa <camilo.saa@gmail.com>
2022-10-30 01:52:57 +0200

    moving logic to the single card component and removing the eslintrc

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import './App.css';
+import SingleCard from './components/SingleCard.js';
 
 const cardImages = [
   { src: 'img/helmet-1.png' },
@@ -32,14 +33,11 @@ function App() {
     <div className="App">
       <h1>Magic Match</h1>
       <button onClick={shuffleCards}>New Game</button>
+
       <div className="card-grid">
         {cards.map(card => (
-          <div className="card" key={card.id}>
-            <div>
-              <img className="front" src={card.src} alt="card front" />
-              <img className="back" src="/img/cover.png" alt="card back" />
-            </div>
-          </div>
+          // console.log(card)
+          <SingleCard key={card.id} card={card} />
         ))}
       </div>
     </div>
